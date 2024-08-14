20734fcabbd369a0f230b60a129bb1144abcf394
Emils <135007928+emilsViksnins@users.noreply.github.com>
2024-08-14 10:22:18 +0300

    Initial commit

---
+++ src/test/java/dev/emils/CustomLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
